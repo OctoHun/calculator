e705c40ccfc8ceadded3545b9925de06177b7ea2
Pandemic23 <popofh12345@naver.com>
2023-08-31 23:09:35 +0900

    Day4 Maven framework

--- Min.java
+++
@@ -1,5 +0,0 @@
-public final class Min {
-    public final static int Min(int a, int b) {
-        return a-b;
-    }
-}
--- Sum.java
+++
@@ -1,5 +0,0 @@
-public final class Sum {
-    public final static int Sum(int a, int b) {
-        return a + b;
-    }
-}
